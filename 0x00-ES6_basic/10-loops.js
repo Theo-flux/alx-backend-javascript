1254f70d087e07c2c5a9e408269c5c4aae02c201
Theo <ekunnusitheophilus@gmail.com>
2023-01-25 16:23:59 +0100

    [refactor]: For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable guard-for-in */
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const idx of array) {
+    const value = idx;
+    array[i] = appendString + value;
+    i += 1;
+  }
+
+  return array;
+}
