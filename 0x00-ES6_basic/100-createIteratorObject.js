d9e726cbb99f24614827cd685af04c8ccb278792
Theo <ekunnusitheophilus@gmail.com>
2023-06-27 01:05:46 +0100

    fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,7 @@
 export default function createIteratorObject(report) {
-  const { allEmployees } = report;
-  return [...allEmployees.engineering, ...allEmployees.marketing];
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
