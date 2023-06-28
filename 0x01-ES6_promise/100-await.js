67fe6c6475af5da9d7f86c0f007eec29cb37dae1
Theo <ekunnusitheophilus@gmail.com>
2023-06-28 12:52:11 +0100

    fix: what could be the hold!

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,10 +4,6 @@ export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
 
-  if (!photo || !user) {
-    throw new Error('');
-  }
-
   try {
     return { photo, user };
   } catch (e) {
