f108f71f19e6194f7834f428f83d2654d40cf157
Theo <ekunnusitheophilus@gmail.com>
2023-06-29 15:48:05 +0100

    A Building; an abstract one.

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(arg) {
+    this._sqft = arg;
+  }
+}
