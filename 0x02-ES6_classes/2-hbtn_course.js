46a638f0006d80101138a54a0668a934f0e7e735
Theo <ekunnusitheophilus@gmail.com>
2023-06-29 13:25:51 +0100

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    else {
+      this._name = name;
+    }
+
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    else {
+      this._length = length;
+    }
+
+    if (students.find((student) => typeof student !== 'string')) throw new TypeError('students must be string');
+    else {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw new TypeError('Name must be a string');
+    else {
+      this._name = newName;
+    }
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw new TypeError('Length must be a number');
+    else {
+      this._length = newLength;
+    }
+  }
+
+  get students() {
+    return this.students;
+  }
+
+  set students(newStudents) {
+    if (newStudents.find((student) => typeof student !== 'string')) throw new TypeError('students must be string');
+    else {
+      this._students = newStudents;
+    }
+  }
+}
