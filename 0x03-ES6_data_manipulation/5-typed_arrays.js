81487cef760d3aebe2c451cee5fe815639a68aec
Theo <ekunnusitheophilus@gmail.com>
2023-07-04 16:50:19 +0100

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const intView = new Int8Array(buffer, 0, length);
-  intView[position] = value;
+  const int8View = new Int8Array(buffer, 0, length);
+  int8View[position] = value;
 
   return new DataView(buffer);
 }
