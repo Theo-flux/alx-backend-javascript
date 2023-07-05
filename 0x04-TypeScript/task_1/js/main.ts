3cd16e3a714bc3e1a46387b6a47ef198b29225fe
Theo <ekunnusitheophilus@gmail.com>
2023-07-05 14:42:03 +0100

    clean up

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,43 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: unknown;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+const printTeacher = (
+  firstName: printTeacherFunction['firstName'],
+  lastName: printTeacherFunction['lastName']
+): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
