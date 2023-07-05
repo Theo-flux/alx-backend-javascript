03cd117870217f8bb120c9988c555add04e41cca
Theo <ekunnusitheophilus@gmail.com>
2023-07-05 01:23:23 +0100

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,12 @@
-/* eslint-disable */
 export default function cleanSet(setArg, startString) {
   const arr = [...setArg];
-  let abridgedArr = [];
-  const startStringLen = startString.length;
+  const startStrLen = startString.length;
 
-  if (startString === undefined || startString.length === 0) {
+  if (startString === undefined || startStrLen === 0) {
     return '';
   }
-  abridgedArr = arr
-    .filter(el => (el !== undefined ? el.startsWith(startString) : ''))
-    .map(el => (el !== undefined ? el.slice(startStringLen) : ''))
+  return arr
+    .filter((el) => (el !== undefined ? el.slice(0, startStrLen) : ''))
+    .map((el) => (el !== undefined ? el.slice(startStrLen) : ''))
     .join('-');
-
-  return abridgedArr;
 }
