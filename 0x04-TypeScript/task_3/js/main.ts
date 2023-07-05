57294553a1c3fac26b9c3a7a8f952d701a2fa1fd
Theo <ekunnusitheophilus@gmail.com>
2023-07-05 17:22:36 +0100

    ambient, triple-slash concept for lib implementation

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,80 +1,15 @@
-interface DirectorInterface {
-  workFromHome: () => string;
-  getCoffeeBreak: () => string;
-  workDirectorTasks: () => string;
-}
+/// <reference path='crud.d.ts'/>
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-interface TeacherInterface {
-  workFromHome: () => string;
-  getCoffeeBreak: () => string;
-  workTeacherTasks: () => string;
-}
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
 
-class Director implements DirectorInterface {
-  workFromHome() {
-    return 'working from home';
-  }
+const newRowID: RowID = CRUD.insertRow(row);
+row.age = 23;
+const updateRow: RowElement = row;
 
-  getCoffeeBreak() {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks() {
-    return 'Getting to director tasks';
-  }
-}
-
-class Teacher implements TeacherInterface {
-  workFromHome() {
-    return 'Cannot work from home';
-  }
-
-  workTeacherTasks() {
-    return 'Getting to work';
-  }
-
-  getCoffeeBreak() {
-    return 'Cannot have a break';
-  }
-}
-
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  } else if (typeof salary === 'string') {
-    salary = parseInt(salary.slice(1));
-    console.log(salary);
-
-    if (salary < 500) {
-      return new Teacher();
-    }
-  }
-
-  return new Director();
-}
-
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
-
-function isDirector(employee: Director | Teacher): boolean {
-  return employee instanceof Director;
-}
-
-function executeWork(employee: Director | Teacher): string {
-  if (employee instanceof Teacher) {
-    return employee.workTeacherTasks();
-  }
-  return employee.workDirectorTasks();
-}
-
-console.log(executeWork(createEmployee(200)));
-console.log(executeWork(createEmployee(1000)));
-
-function teachClass(subject: 'Math' | 'History'): string {
-  if (subject === 'Math') return `Teaching ${subject}`;
-  return `Teaching ${subject}`;
-}
-
-console.log(teachClass('Math'));
-console.log(teachClass('History'));
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);
