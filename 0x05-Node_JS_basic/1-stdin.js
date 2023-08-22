ea84e7ef92c1935353b7c3b62f0d808f0c32b571
Theo <ekunnusitheophilus@gmail.com>
2023-08-22 20:45:20 +0100

    fix: Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (name) => {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name.toString()}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (name) => {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name.toString()}`);
     process.exit();
   });
   process.on('exit', () => {
