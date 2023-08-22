65ef2d2d89977de6f8dadf8f1dc7ffaf01d10501
Theo <ekunnusitheophilus@gmail.com>
2023-08-22 22:14:19 +0100

    fix: fix for req

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let course = {};
+  const course = {};
   let NUMBER_OF_STUDENT = 0;
   try {
     const content = fs.readFileSync(path, 'utf-8');
@@ -21,14 +21,15 @@ function countStudents(path) {
 
     NUMBER_OF_STUDENT = NUMBER_OF_STUDENT;
 
-    console.log('Number of course: ' + NUMBER_OF_STUDENT--);
+    console.log(`Number of course: ${NUMBER_OF_STUDENT--}`);
     for (const [key, value] of Object.entries(course)) {
-      if (key !== 'field')
+      if (key !== 'field') {
         console.log(
           `Number of students in ${key}: ${value.length}. List: ${value.join(
-            ', '
-          )}`
+            ', ',
+          )}`,
         );
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
