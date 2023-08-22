57882997c741edcff53843d47de0bc99db572b8b
Theo <ekunnusitheophilus@gmail.com>
2023-08-22 22:12:27 +0100

    fix: Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,41 +1,37 @@
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function countStudents(path) {
+  let course = {};
+  let NUMBER_OF_STUDENT = 0;
   try {
-    const fileContents = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileContents.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
+    const content = fs.readFileSync(path, 'utf-8');
+    const arr = content.toString().split('\n');
+
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i]) {
+        NUMBER_OF_STUDENT += 1;
+        const field = arr[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(course, field[3])) {
+          course[field[3]].push(field[0]);
         } else {
-          students[field[3]] = [field[0]];
-        }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
+          course[field[3]] = [field[0]];
         }
       }
     }
-    const l = length - 1;
-    console.log(`Number of students: ${l}`);
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
+
+    NUMBER_OF_STUDENT = NUMBER_OF_STUDENT;
+
+    console.log('Number of course: ' + NUMBER_OF_STUDENT--);
+    for (const [key, value] of Object.entries(course)) {
+      if (key !== 'field')
         console.log(
-          `Number of students in ${key}: ${value}. List: ${students[key].join(
+          `Number of students in ${key}: ${value.length}. List: ${value.join(
             ', '
           )}`
         );
-      }
     }
   } catch (error) {
-    throw Error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 
