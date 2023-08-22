0033746885e28b89c649c7aec574d698c60a12d2
Theo <ekunnusitheophilus@gmail.com>
2023-08-22 22:34:31 +0100

    fix: async file system

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,43 +1,38 @@
-const fs = require('fs');
+const { readFile } = require('fs');
 
-function countStudents(path) {
+function countcourse(fileName) {
+  const course = {};
+  let length = 0;
   return new Promise((resolve, reject) => {
-    fs.readFile(path, (error, data) => {
+    readFile(fileName, (error, data) => {
       if (error) {
-        reject(error);
-      }
-      const course = {};
-      let NUMBER_OF_STUDENT = 0;
-
-      const arr = data.toString().split('\n');
-
-      for (let i = 0; i < arr.length; i += 1) {
-        if (arr[i]) {
-          NUMBER_OF_STUDENT += 1;
-          const field = arr[i].toString().split(',');
-          if (Object.prototype.hasOwnProperty.call(course, field[3])) {
-            course[field[3]].push(field[0]);
-          } else {
-            course[field[3]] = [field[0]];
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(course, field[3])) {
+              course[field[3]].push(field[0]);
+            } else {
+              course[field[3]] = [field[0]];
+            }
           }
         }
-      }
-
-      NUMBER_OF_STUDENT = NUMBER_OF_STUDENT - 1;
-
-      console.log(`Number of course: ${NUMBER_OF_STUDENT}`);
-      for (const [key, value] of Object.entries(course)) {
-        if (key !== 'field') {
-          console.log(
-            `Number of students in ${key}: ${value.length}. List: ${value.join(
-              ', '
-            )}`
-          );
+        const l = length - 1;
+        console.log(`Number of course: ${l}`);
+        for (const [key, value] of Object.entries(course)) {
+          if (key !== 'field') {
+            console.log(
+              `Number of course in ${key}: ${value}. List: ${value.join(', ')}`
+            );
+          }
         }
+        resolve(data);
       }
-      resolve(data);
     });
   });
 }
 
-module.exports = countStudents;
+module.exports = countcourse;
