0286348856653d052399e8fa05fc2015f8f8a6d6
Theo <ekunnusitheophilus@gmail.com>
2023-08-22 20:36:32 +0100

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('readable', () => {
+    const name = process.stdin.read()
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('readable', () => {
+    const name = process.stdin.read()
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
